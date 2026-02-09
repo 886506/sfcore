fff69a3dfd91d7ee492897428c37e03b6b0a7ca4
Puneet <Puneetsaini294@gmail.com>
2026-02-09 13:59:58 +0530

    console added

--- Salesforce/force-app/main/default/lwc/myFirstLWC/myFirstLWC.js
+++ Salesforce/force-app/main/default/lwc/myFirstLWC/myFirstLWC.js
@@ -1,3 +1,7 @@
 import { LightningElement } from 'lwc';
 
-export default class MyFirstLWC extends LightningElement {}
+export default class MyFirstLWC extends LightningElement {
+    connectedCallback(){
+        console.log('6::');
+    }
+}
